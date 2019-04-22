e038edbab894c1429f43e8aac3ba60e9399e4cf5
andrew kadry <andrew.raghib@gmail.com>
2019-04-22 16:15:36 +0200

    first

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'E-collect' });
 });
 
 router.get('/request', function(req, res, next) {
-  res.render('error', { title: 'Express' });
+  res.render('error', { title: 'E-collect' });
+});
+router.get('/about', function(req, res, next) {
+    res.render('about', { title: 'E-collect' });
+});
+router.get('/home', function(req, res, next) {
+    res.render('index', { title: 'E-collect' });
 });
 
 module.exports = router;
